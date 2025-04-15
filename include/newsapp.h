af992776687155f5d36c192e62ada575a3bc9361
victorthedude <victortruong00@gmail.com>
2025-04-15 14:34:15 +0200

    add NewsApp class + mock impl

---
+++ include/newsapp.h
@@ -0,0 +1,43 @@
+#ifndef NEWSAPP_H
+#define NEWSAPP_H
+
+#include "database.h"
+#include "messageHandler.h"
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+class NewsApp {
+    public:
+        // initializes a Server on 'port' with Database 'db'
+        // NewsApp(Server* serv, Database* db);
+        // NewsApp(MessageHandler mh, Database* db);
+        NewsApp(Database* db, MessageHandler mh);
+
+        void process_request(std::shared_ptr<Connection>& conn);
+
+        /* Used in main server loop? Or remove and outsource to main file? */
+        void serve_client();
+
+        vector<string> list_groups();
+        // TODO: add suitable parameters
+        // TODO: change return value from 'bool' to 'int'? (for error codes)
+        bool create_group();
+        bool delete_group();
+        
+        // TODO: add suitable parameters
+        // TODO: change return value from 'bool' to 'int'? (for error codes)
+        vector<string> list_articles();
+        bool create_article();
+        bool delete_article();
+        
+    private:
+        Database* database; // obs: must be pointer or ref when abstract class
+        MessageHandler messageHandler;
+
+};
+
+#endif
---
+++ src/newsapp.cc
@@ -0,0 +1,42 @@
+
+#include "newsapp.h"
+// #include "database.h"
+#include "connection.h"
+
+#include <iostream>
+#include <string>
+// NewsApp::NewsApp(Server* serv, Database* db) {
+//     server = serv;
+//     database = db;
+// }
+
+// NewsApp::NewsApp(MessageHandler mh, Database* db) {
+//     mh = mh;
+//     database = db;
+// }
+
+NewsApp::NewsApp(Database* db, MessageHandler mh) {
+    database = db;
+    messageHandler = mh;
+}
+
+int readNumber(const std::shared_ptr<Connection>& conn)
+{
+    unsigned char byte1 = conn->read();
+    unsigned char byte2 = conn->read();
+    unsigned char byte3 = conn->read();
+    unsigned char byte4 = conn->read();
+    return (byte1 << 24) | (byte2 << 16) | (byte3 << 8) | byte4;
+}
+
+
+
+void NewsApp::process_request(std::shared_ptr<Connection>& conn) {
+    // MOCK IMPL: read and return integer from client.
+    int nbr = readNumber(conn);
+    std::string s = std::to_string(nbr);
+    for (char c : s) {
+        conn->write(c);
+    }
+    conn->write('$');
+}
